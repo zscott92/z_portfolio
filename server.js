8abc06dcdf31abf29efd5065ecaf5323882713cc
zscott92@gmail.com <zscott92@gmail.com>
2019-09-09 22:46:35 -0400

    updated

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+
 const express = require("express");
 
 const app = express();
@@ -9,10 +10,10 @@ var PORT = process.env.PORT || 8080;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(express.static("public"));
+app.use(express.static("./public"));
 
-require("./htmlroutes.js")(app);
+require("./routes/htmlroutes")(app);
 
 app.listen(PORT, function () {
     console.log("App listening on PORT " + PORT);
-});
+})
